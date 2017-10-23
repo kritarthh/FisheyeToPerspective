1adbd05c6275a0ec904a2b92fc0fc815ed511e88
Kritarth <me@kritarth.net>
2017-10-23 21:08:53 +0530

    convert images in folders recursively

--- fishToPersp.cpp
+++ fishToPersp.cpp
@@ -3,14 +3,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <dirent.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
+
 using namespace std;
 using namespace cv;
-#define PI 3.1415926536
 
+#define PI 3.1415926536
 #define CMV_MAX_BUF 1024
 #define MAX_POL_LENGTH 64
 
@@ -29,7 +31,7 @@ struct ocam_model
     int    height;                 // image height
 };
 
-int get_ocam_model(struct ocam_model *myocam_model, char *filename)
+int get_ocam_model(struct ocam_model *myocam_model, const char *filename)
 {
     double *pol           = myocam_model->pol;
     double *invpol        = myocam_model->invpol; 
@@ -180,8 +182,7 @@ void world2cam(double point2D[2], double point3D[3], struct ocam_model *myocam_m
         rho = invpol[0];
         t_i = 1;
 
-        for (i = 1; i < length_invpol; i++)
-        {
+        for (i = 1; i < length_invpol; i++) {
             t_i *= t;
             rho += t_i*invpol[i];
         }
@@ -221,91 +222,195 @@ void create_perspecive_undistortion_LUT( CvMat *mapx, CvMat *mapy, struct ocam_m
         }
 }
 
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
+struct fish_span {
+    float hor_left;
+    float hor_right;
+    int hor_cnt;
+    float vir_down;
+    float vir_up;
+    int vir_cnt;
+};
+
+void fishToPersp(string path, fish_span& fs, ocam_model& o)
+{
+    Mat origImage = imread(path);
+    replace(path, "/input/", "/output/");
+
+    if (origImage.empty()) {
+        cout<<"Empty image\n";
+        return;
+    }
+    float hor, vir;
+    for (int h = 0; h < fs.hor_cnt; ++h)
+    {
+        for (int v = 0; v < fs.vir_cnt; ++v)
+        {
+            if (fs.hor_cnt == 1) hor = fs.hor_left;
+            else hor = fs.hor_left + ((fs.hor_right - fs.hor_left)/(fs.hor_cnt - 1)) * h;
+            if (fs.vir_cnt == 1) vir = fs.vir_down;
+            else vir = fs.vir_down + ((fs.vir_up - fs.vir_down)/(fs.vir_cnt - 1)) * v;
+            string newPath = path.substr(0, path.size() - 4)
+                           + "_"
+                           + to_string(hor).substr(0, to_string(hor).size() - 5)
+                           + "_deg_x_"
+                           + to_string(vir).substr(0, to_string(vir).size() - 5)
+                           + "_deg_y.jpg";
+            
+            cout << newPath.substr(path.find_last_of("/") + 1) << endl;
+            if (FILE *file = fopen(newPath.c_str(), "r")) {
+                fclose(file);
+                continue;
+            }
+
+            Mat orignalImage = origImage;
+            float theta = hor * PI / 180;
+            float phi = vir * PI / 180;
+
+            Mat outImage(orignalImage.rows,orignalImage.cols,CV_8UC3);
+
+            Point2f center;
+            center.x = orignalImage.cols >> 1;
+            center.y = orignalImage.rows >> 1;
+
+            for(int i = 0; i < outImage.cols; i++)
+            {
+                for(int j = 0; j < outImage.rows; j++)
+                {
+                    Point2f inP =  getInputPoint(i,j,orignalImage.cols,orignalImage.rows, center, theta, phi);
+                    Point inP2((int)inP.x,(int)inP.y);
+                    if(inP2.x >= orignalImage.cols || inP2.y >= orignalImage.rows)
+                        continue;
+                    if(inP2.x < 0 || inP2.y < 0)
+                        continue;
+                    Vec3b color = orignalImage.at<Vec3b>(inP2);
+                    outImage.at<Vec3b>(Point(i,j)) = color;
+                }
+            }
+
+            orignalImage = outImage.clone();
+
+            for(int i = 0; i < outImage.cols; i++)
+            {
+                for(int j = 0; j < outImage.rows; j++)
+                {
+                    Point2f inP =  revGetInputPoint(i,j,orignalImage.cols,orignalImage.rows, center);
+                    Point inP2((int)inP.x,(int)inP.y);
+                    if(inP2.x >= orignalImage.cols || inP2.y >= orignalImage.rows)
+                        continue;
+                    if(inP2.x < 0 || inP2.y < 0)
+                        continue;
+                    Vec3b color = orignalImage.at<Vec3b>(inP2);
+                    outImage.at<Vec3b>(Point(i,j)) = color;
+                }
+            }
+
+            double point3D[3];
+            double point2D[2];
+
+            IplImage *src = cvCreateImage(cvSize(orignalImage.cols, orignalImage.rows), 8, 3);
+            // src->imageData = (char *) outImage.data;
+            memcpy(src->imageData, outImage.data, orignalImage.cols * orignalImage.rows * 3);
+
+            IplImage *dst_persp = cvCreateImage(cvGetSize(src), 8, 3);
+            CvMat* mapx_persp   = cvCreateMat(src->height, src->width, CV_32FC1);
+            CvMat* mapy_persp   = cvCreateMat(src->height, src->width, CV_32FC1);
+
+            float sf = 4.0;
+            create_perspecive_undistortion_LUT(mapx_persp, mapy_persp, &o, sf);
+            cvRemap(src, dst_persp, mapx_persp, mapy_persp, CV_INTER_LINEAR+CV_WARP_FILL_OUTLIERS, cvScalarAll(0));
+
+            // crop image
+            int offset_x = (src->width - src->height) >> 1;
+
+            cvSetImageROI(dst_persp, cvRect(offset_x, 0, src->height, src->height));
+
+            IplImage *tmp = cvCreateImage(cvGetSize(dst_persp),
+                                          dst_persp->depth,
+                                          dst_persp->nChannels);
+
+            cvCopy(dst_persp, tmp, NULL);
+
+            cvSaveImage(newPath.c_str(), tmp);
+
+            cvReleaseImage(&src);
+            cvReleaseImage(&dst_persp);
+            cvReleaseImage(&tmp);
+            cvReleaseMat(&mapx_persp);
+            cvReleaseMat(&mapy_persp);
+            outImage.release();
+            orignalImage.release();
+        }
+    }
+    origImage.release();
+    return;
+}
+
+int listdir(string path, fish_span& fs, ocam_model& o)
+{
+    struct dirent *entry;
+    DIR *dp;
+
+    dp = opendir(path.c_str());
+    if (dp == NULL) {
+        // perror("opendir: Path does not exist or could not be read.");
+        return -1;
+    }
+
+    // create output folder if doesnot exist
+    replace(path, "/input/", "/output/");
+    system(("mkdir -p " + path).c_str());
+    replace(path, "/output/", "/input/");
+
+    while ((entry = readdir(dp))) {
+        string name = string(entry->d_name);
+        if (name == "." || name == "..") {
+            continue;
+        }
+        if (name.size() > 4 && name.substr(name.size() - 4) == ".jpg") {
+            cout << "Image found: " << name << endl;
+            fishToPersp(path + "/" + name, fs, o);
+        } else {
+            listdir(path + "/" + name, fs, o);
+        }
+    }
+    closedir(dp);
+    return 0;
+}
+
+
 int main(int argc, char **argv)
 {
-    if (argc < 5) {
-        cout << "Usage: ./fishToPersp <calibration_file> <input_image> <horizontal_viewangle> <vertical_viewangle>\n";
+    if (argc < 7) {
+        cout << "Usage: ./fishToPersp <point_to_directory_containing_files> <horizontal_viewangle: left right count> <vertical_viewangle: down up count>\n";
         return 0;
     }
 
-    struct ocam_model o, o_cata;
-    get_ocam_model(&o, argv[1]);
+    struct ocam_model o;
+    get_ocam_model(&o, (string(argv[1]) + "/files/calib.txt").c_str());
+    
+    struct fish_span fs;
+    fs.hor_left = strtof(argv[2], 0);
+    fs.hor_right = strtof(argv[3], 0);
+    fs.hor_cnt = stoi(argv[4]);
+    fs.vir_down = strtof(argv[5], 0);
+    fs.vir_up = strtof(argv[6], 0);
+    fs.vir_cnt = stoi(argv[7]);
 
-    Mat orignalImage = imread(argv[2]);
+    cout << fs.hor_left << " " << fs.hor_right << " " << fs.hor_cnt << " " << fs.vir_down << " " << fs.vir_up << " " << fs.vir_cnt << endl;
 
-    if (orignalImage.empty()) {
-        cout<<"Empty image\n";
-        return 0;
-    }
+    string dirPath = string(realpath(argv[1], NULL)) + "/files/input/";
 
-    float theta = strtof(argv[3],0) * PI / 180;
-    float phi = strtof(argv[4],0) * PI / 180;
+    listdir(dirPath, fs, o);
 
-    Mat outImage(orignalImage.rows,orignalImage.cols,CV_8UC3);
 
-    Point2f center;
-    center.x = orignalImage.cols >> 1;
-    center.y = orignalImage.rows >> 1;
-
-    for(int i = 0; i < outImage.cols; i++)
-    {
-        for(int j = 0; j < outImage.rows; j++)
-        {
-            Point2f inP =  getInputPoint(i,j,orignalImage.cols,orignalImage.rows, center, theta, phi);
-            Point inP2((int)inP.x,(int)inP.y);
-            if(inP2.x >= orignalImage.cols || inP2.y >= orignalImage.rows)
-                continue;
-            if(inP2.x < 0 || inP2.y < 0)
-                continue;
-            Vec3b color = orignalImage.at<Vec3b>(inP2);
-            outImage.at<Vec3b>(Point(i,j)) = color;
-        }
-    }
-
-    orignalImage = outImage.clone();
-
-    for(int i = 0; i < outImage.cols; i++)
-    {
-        for(int j = 0; j < outImage.rows; j++)
-        {
-            Point2f inP =  revGetInputPoint(i,j,orignalImage.cols,orignalImage.rows, center);
-            Point inP2((int)inP.x,(int)inP.y);
-            if(inP2.x >= orignalImage.cols || inP2.y >= orignalImage.rows)
-                continue;
-            if(inP2.x < 0 || inP2.y < 0)
-                continue;
-            Vec3b color = orignalImage.at<Vec3b>(inP2);
-            outImage.at<Vec3b>(Point(i,j)) = color;
-        }
-    }
-
-    double point3D[3];
-    double point2D[2];
-
-    IplImage *src       = new IplImage(outImage);
-    IplImage *dst_persp = cvCreateImage(cvGetSize(src), 8, 3);
-    CvMat* mapx_persp   = cvCreateMat(src->height, src->width, CV_32FC1);
-    CvMat* mapy_persp   = cvCreateMat(src->height, src->width, CV_32FC1);
-
-    float sf = 4.0;
-    create_perspecive_undistortion_LUT( mapx_persp, mapy_persp, &o, sf );
-    cvRemap(src, dst_persp, mapx_persp, mapy_persp, CV_INTER_LINEAR+CV_WARP_FILL_OUTLIERS, cvScalarAll(0));
-
-    // crop image
-    int offset_x = (src->width - src->height) >> 1;
-
-    cvSetImageROI(dst_persp, cvRect(offset_x, 0, src->height, src->height));
-
-    IplImage *tmp = cvCreateImage(cvGetSize(dst_persp),
-                                  dst_persp->depth,
-                                  dst_persp->nChannels);
-
-    cvCopy(dst_persp, tmp, NULL);
-    cvResetImageROI(dst_persp);
-
-    dst_persp = cvCloneImage(tmp);
-
-    cvSaveImage((string(argv[3]) + "_deg_x_" + string(argv[4]) + "_deg_y.jpg").c_str(), dst_persp);
 
     return 0;
 }
